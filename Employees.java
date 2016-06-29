77566e1ef58922911d1fa47a71837390334f8c69
rahulnaamtohsunnahoga <h.aither1991@gmail.com>
2016-06-29 11:29:25 +0530

    Create Employees.java

---
+++ Employees.java
@@ -0,0 +1,44 @@
+package com.tcs.codetest;
+
+/**
+ * Created by 983798 on 6/28/2016.
+ */
+public class Employees {
+
+    private String mID;
+    private String mName;
+    private String mDept;
+    private String mPhone;
+
+    public String getmID() {
+        return mID;
+    }
+
+    public void setmID(String mID) {
+        this.mID = mID;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmDept() {
+        return mDept;
+    }
+
+    public void setmDept(String mDept) {
+        this.mDept = mDept;
+    }
+
+    public String getmPhone() {
+        return mPhone;
+    }
+
+    public void setmPhone(String mPhone) {
+        this.mPhone = mPhone;
+    }
+}
